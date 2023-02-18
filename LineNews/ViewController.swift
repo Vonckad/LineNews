e5851d6774611ce6c469465af636cea03ccec420
vonkad <Vonkad@yandex.ru>
2023-02-18 12:02:05 +0300

    Initial Commit

---
+++ LineNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LineNews
+//
+//  Created by Vlad Ralovich on 18.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
